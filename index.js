9c70f403122d8e3642b0865d7825ed835c707bf4
HectorsLittleDevil <38656944+HectorsLittleDevil@users.noreply.github.com>
2018-05-19 23:22:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -105,23 +105,6 @@ if(cmd === `${prefix}ban`){
       return;
     }
 
-
-
-  if(cmd === `${prefix}serverinfo`){
-
-    let sicon = message.guild.IconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    return message.channel.send(serverembed);
-  }
-
   if(cmd === `${prefix}botinfo`){
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
